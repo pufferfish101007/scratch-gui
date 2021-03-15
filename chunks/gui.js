be576fabc52a09b23a713029ae17795af591f36e
pufferfish101007 <pufferfish101007@users.noreply.github.com>
2021-03-15 18:17:59 +0000

    Deploying to gh-pages from  @ 279bf52689191a6fbab063bfb7157ce9bdb54e74 🚀

--- chunks/gui.js
+++ chunks/gui.js
@@ -147,7 +147,7 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 
 
 var onClickLogo = function onClickLogo() {
-  alert("hello there");
+  window.location = 'https://scratch.mit.edu';
 };
 
 var handleTelemetryModalCancel = function handleTelemetryModalCancel() {
